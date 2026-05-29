7ba7c03014dc8a71f9fe151c930c6d865e235363
Remo <1742807707@qq.com>
2026-05-29 17:17:12 +0800

    添加springboot初始应用

---
+++ src/main/java/com/hdg/prysm/PrysmApplication.java
@@ -0,0 +1,13 @@
+package com.hdg.prysm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrysmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrysmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdg/prysm/PrysmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdg.prysm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrysmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
